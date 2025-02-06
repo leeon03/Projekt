38bc3da10056813680ec98b4f2872093bb3063da
leeon03 <leonnutz@yahoo.com>
2025-02-06 21:04:37 +0100

    Neuster Stand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"], // Stellt sicher, dass Tailwind alle HTML- und JS-Dateien scannt
+  darkMode: 'class', // Darkmode über Klasse aktivieren
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2A9D8F",
+        secondary: "#E76F51",
+        accent: "#F4A261",
+        background: {
+          light: "#F4F4F4",
+          dark: "#1E1E1E"
+        },
+        text: {
+          light: "#333333",
+          dark: "#EAEAEA"
+        },
+        hover: "#264653"
+      }
+    }
   },
   plugins: [],
 }
